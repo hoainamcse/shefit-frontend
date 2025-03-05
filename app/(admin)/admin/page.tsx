a92029eea7aff84dc7166aa1e07ac53ae0ae1cf1
Nam Nguyen <hoainamcse@gmail.com>
2025-03-05 14:46:17 +0700

    feat: add admin layout

---
+++ app/(admin)/admin/page.tsx
@@ -0,0 +1,5 @@
+import { ContentLayout } from '@/components/admin-panel/content-layout'
+
+export default function AdminPage() {
+  return <ContentLayout title="Trang chủ">Content Input for Trang chủ</ContentLayout>
+}
