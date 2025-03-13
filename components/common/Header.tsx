f924d74940a0326b207ee82a2cbf5b709fe415ec
wolf4dev <phuocnguyencloud9@gmail.com>
2025-03-13 16:31:31 +0700

    fix about icon

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,13 +1,14 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { ExerciseYogaIcon } from "@/components/icons/ExerciseYogaIcon";
-import { FoodGrainsIcon } from "@/components/icons/FoodGrainsIcon";
-import { GymIcon } from "@/components/icons/GymIcon";
-import { FitnessLineIcon } from "@/components/icons/FitnessLineIcon";
-import { BookIcon } from "@/components/icons/BookIcon";
-import { AccountIcon } from "@/components/icons/AccountIcon";
-import { MenuIcon } from "@/components/icons/MenuIcon";
+import Image from "next/image"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+import { ExerciseYogaIcon } from "@/components/icons/ExerciseYogaIcon"
+import { FoodGrainsIcon } from "@/components/icons/FoodGrainsIcon"
+import { GymIcon } from "@/components/icons/GymIcon"
+import { FitnessLineIcon } from "@/components/icons/FitnessLineIcon"
+import { BookIcon } from "@/components/icons/BookIcon"
+import { AboutIcon } from "@/components/icons/AboutIcon"
+import { AccountIcon } from "@/components/icons/AccountIcon"
+import { MenuIcon } from "@/components/icons/MenuIcon"
 import {
   Sheet,
   SheetContent,
@@ -16,7 +17,7 @@ import {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-} from "@/components/ui/sheet";
+} from "@/components/ui/sheet"
 
 export default function Header() {
   const navItems = [
@@ -42,7 +43,7 @@ export default function Header() {
     },
     {
       label: "Về Shefit",
-      icon: FitnessLineIcon,
+      icon: AboutIcon,
       url: "/about",
     },
     {
@@ -55,32 +56,19 @@ export default function Header() {
       icon: AccountIcon,
       url: "/account",
     },
-  ];
+  ]
 
-  const contactButton = (
-    <Button className="rounded-2xl bg-button hover:bg-[#11c296]">
-      Liên hệ với chúng tôi
-    </Button>
-  );
+  const contactButton = <Button className="rounded-2xl bg-button hover:bg-[#11c296]">Liên hệ với chúng tôi</Button>
 
   return (
     <header className="bg-primary sticky top-0 z-50">
       <div className="max-w-screen-xl mx-auto flex justify-between items-center p-3">
         <Link href="/">
-          <Image
-            src="/logo-light.png"
-            alt="logo-light"
-            width={136}
-            height={40}
-          />
+          <Image src="/logo-light.png" alt="logo-light" width={136} height={40} />
         </Link>
         <div className="justify-center items-center gap-6 text-background hidden md:flex">
           {navItems.map((item, index) => (
-            <Link
-              key={`navItem-${index}`}
-              href={item.url}
-              className="flex flex-col items-center gap-1"
-            >
+            <Link key={`navItem-${index}`} href={item.url} className="flex flex-col items-center gap-1">
               <item.icon />
               {item.label}
             </Link>
@@ -95,18 +83,12 @@ export default function Header() {
           </SheetTrigger>
           <SheetContent className="max-w-sm">
             <SheetHeader>
-              <SheetTitle className="text-text uppercase text-xl font-bold">
-                Menu
-              </SheetTitle>
+              <SheetTitle className="text-text uppercase text-xl font-bold">Menu</SheetTitle>
               <SheetDescription />
             </SheetHeader>
             <div>
               {navItems.map((item, index) => (
-                <Link
-                  key={`navItem-${index}`}
-                  href={item.url}
-                  className="flex items-center gap-1 mb-3"
-                >
+                <Link key={`navItem-${index}`} href={item.url} className="flex items-center gap-1 mb-3">
                   <item.icon />
                   {item.label}
                 </Link>
@@ -117,5 +99,5 @@ export default function Header() {
         </Sheet>
       </div>
     </header>
-  );
+  )
 }
