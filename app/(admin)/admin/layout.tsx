f5a5ce7c9c745247294ac00dadd743175f5c50d2
Nam Nguyen <hoainamcse@gmail.com>
2025-06-11 11:48:23 +0700

    Chore: remove logic was handled by middleware

--- app/(admin)/admin/layout.tsx
+++ app/(admin)/admin/layout.tsx
@@ -1,14 +1,5 @@
-import { redirect } from 'next/navigation'
-
 import AdminPanelLayout from '@/components/admin-panel/admin-panel-layout'
-import { verifySession } from '@/lib/dal'
 
 export default async function AdminLayout({ children }: { children: React.ReactNode }) {
-  const session = await verifySession()
-
-  if (!session) {
-    redirect('/auth/login')
-  }
-
   return <AdminPanelLayout>{children}</AdminPanelLayout>
 }
