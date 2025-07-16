86f0dcb31476314616c611ea9f994c917d1cb813
Thanh Tâm <tam16121612@gmail.com>
2025-07-16 04:14:13 +0000

    Fix: padding responsive of about, blog, meal-plans pages
    * Fix: padding responsive of about, blog, meal-plans pages

--- app/(user)/about/page.tsx
+++ app/(user)/about/page.tsx
@@ -13,7 +13,7 @@ export default async function About() {
   }
 
   return (
-    <div className="flex flex-col gap-10 mt-10 xl:p-10 max-lg:p-0">
+    <div className="flex flex-col gap-10 mt-10 p-4 sm:p-6 md:p-8 xl:p-10">
       <img src={aboutUsData['thumbnail_image']} alt="About Us" className="w-full h-auto max-h-[680px] object-cover rounded-lg" />
       <div>
         <div className="font-[family-name:var(--font-coiny)] xl:text-[40px] text-3xl font-bold mb-6">Về Shefit</div>
--- app/(user)/blog/layout.tsx
+++ app/(user)/blog/layout.tsx
@@ -3,7 +3,7 @@ import Layout from '@/app/(user)/_components/layout'
 export default async function BlogLayout({ children }: { children: React.ReactNode }) {
   return (
     <Layout>
-      <div className="lg:p-6 max-w-screen-3xl mx-auto mb-20">{children}</div>
+      <div className="p-4 sm:p-6 md:p-8 xl:p-10 max-w-screen-3xl mx-auto mb-20">{children}</div>
     </Layout>
   )
 }
